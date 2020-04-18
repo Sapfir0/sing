418b25974760734becf2a12923a18750a1c3d73f
Sapfir0 <sapfir999999@yandex.ru>
2020-04-18 16:26:58 +0400

    Минимальный сендер

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using System.Net.Mime;
+using System.Text;
+using RabbitMQ.Client.Events;
 
 namespace Converter
 {
@@ -10,6 +12,19 @@ namespace Converter
         {
             
         }*/
+        
+        private void RabbitListener()
+        {
+            model = GetRabbitChannel(exchangeName, queueName, routingKey);
+            var subscription = new Subscription(model, queueName, false);
+            while (true)
+            {
+                BasicDeliverEventArgs basicDeliveryEventArgs = subscription.Next();
+                string messageContent = Encoding.UTF8.GetString(basicDeliveryEventArgs.Body);
+                messagesTextBox.Invoke((MethodInvoker)delegate { messagesTextBox.Text += messageContent + "\r\n"; });
+                subscription.Ack(basicDeliveryEventArgs);
+            }
+        }
         static void Main(string[] args)
         {
             
--- Watcher/Program.cs
+++ Watcher/Program.cs
@@ -1,13 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Permissions;
 using System.Text;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Watcher
 {
+
+    class Sender
+    { 
+        private static IConnection GetRabbitConnection()
+        {
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/",
+                HostName = "localhost"
+            };
+            IConnection conn = factory.CreateConnection();
+            return conn;
+        }
+        private static IModel GetRabbitChannel(string exchangeName, string queueName, string routingKey)
+        {
+            var conn = GetRabbitConnection();
+            var model = conn.CreateModel();
+            model.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            model.QueueDeclare(queueName, false, false, false, null);
+            model.QueueBind(queueName, exchangeName, routingKey, null);
+            return model;
+        }
+        
+        public static void SendMessage(string exchangeName="test", string queueName="test", string routingKey="test")
+        {
+            IModel model = GetRabbitChannel(exchangeName, queueName, routingKey);
+            byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello, world!");
+            model.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+        }
+        
+    }
     class Program
     {
+        /*
+        private Sender sender;
+        */
+
+        public Program()
+        {
+        }
         private byte[] Image2Byte()
         {
             throw new NotImplementedException();
@@ -16,30 +58,27 @@ namespace Watcher
         private static void OnChanged(object source, FileSystemEventArgs e)
         {           
             Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+            Sender.SendMessage();
         }
-
+      
+        
+      
         private static void HandleDirectory(string path)
         {
             using FileSystemWatcher watcher = new FileSystemWatcher();
             watcher.Path = path;
-
-
-            watcher.NotifyFilter = NotifyFilters.LastAccess
-                                   | NotifyFilters.LastWrite
-                                   | NotifyFilters.FileName
-                                   | NotifyFilters.DirectoryName;
-
-
-            watcher.Changed += OnChanged;
+            
             watcher.Created += OnChanged;
-
+            
             watcher.EnableRaisingEvents = true;
+            
+            while(true);
         }
 
 
         static void Main(string[] args)
         {
-            const string subpath = "/incoming";
+            const string subpath = "\\incoming";
             var path = Directory.GetCurrentDirectory() + subpath;
             if (!Directory.Exists(path))
             {
@@ -47,9 +86,12 @@ namespace Watcher
             }
             
             var filesOnStart = Directory.GetFiles(path);
-            var sandedFiles = new List<string>();
+            var sandedFiles = new List<string>(); // можно сохранять уже обработанные файлы
+
+            Console.WriteLine("Детектим изменения в " + path);
 
             HandleDirectory(path);
+
         }
     }
 }
